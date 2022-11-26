243fd1e990535be928ee7df7b9ce2d310fde34eb
Deyson-Trindade <trindadedeyson1@gmail.com>
2022-11-26 20:07:51 -0300

    creating interface to be used in handler layer

---
+++ src/main/java/br/com/insanos/loucadora/film/document/Response.java
@@ -0,0 +1,10 @@
+package br.com.insanos.loucadora.film.document;
+
+public interface Response {
+    String getId();
+    String getTitle();
+    String getSynopsis();
+    String getCategory();
+    String getHrefImage();
+    String getParentalRating();
+}
