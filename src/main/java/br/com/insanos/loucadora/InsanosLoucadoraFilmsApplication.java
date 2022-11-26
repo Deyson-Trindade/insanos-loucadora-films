b43b6dca6ac12eebe670af40552348977ae793ae
Deyson-Trindade <trindadedeyson1@gmail.com>
2022-11-26 16:55:17 -0300

    initial commit

---
+++ src/main/java/br/com/insanos/loucadora/InsanosLoucadoraFilmsApplication.java
@@ -0,0 +1,13 @@
+package br.com.insanos.loucadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsanosLoucadoraFilmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsanosLoucadoraFilmsApplication.class, args);
+    }
+
+}
